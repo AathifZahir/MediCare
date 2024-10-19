43f5281cad0cc38ba8e97f6e45baf4373883e764
aadhilmhmd <aadhilmohammadh991@gmail.com>
2024-10-19 20:58:12 +0530

    Update Profile.jsx

--- src/pages/client/Profile.jsx
+++ src/pages/client/Profile.jsx
@@ -250,7 +250,7 @@ export default function UserProfile() {
             {isEditing ? (
               <button
                 onClick={() => setIsEditing(false)}
-                className="flex-1 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                className="flex-1 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-600 bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
               >
                 Cancel
               </button>
